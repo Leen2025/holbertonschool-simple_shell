021a6ae5b2ef156f1380c6361885a590fc700ded
d-alshehri <10674@holbertonstudents.com>
2025-04-22 16:26:03 +0000

    Fixed betty style getenv.c file

--- getenv.c
+++ getenv.c
@@ -8,13 +8,13 @@
  */
 char **get_environ(info_t *info)
 {
-	if (!info->environ || info->env_changed)
-	{
-		info->environ = list_to_strings(info->env);
-		info->env_changed = 0;
-	}
+if (!info->environ || info->env_changed)
+{
+info->environ = list_to_strings(info->env);
+info->env_changed = 0;
+}
 
-	return (info->environ);
+return (info->environ);
 }
 
 /**
