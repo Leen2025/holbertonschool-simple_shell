5be57c81beed63595d3e36d4f1a276b2da5b0339
d-alshehri <10674@holbertonstudents.com>
2025-04-22 16:19:28 +0000

    Fixed betty style _atoi.c file

--- _atoi.c
+++ _atoi.c
@@ -7,7 +7,7 @@
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 /**
  * is_delim - checks if character is a delimeter
